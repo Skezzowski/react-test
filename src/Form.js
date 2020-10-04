7297af590d5e8651ef00da2d68e7f25c422f8a99
Balazs Sipos <sipblai@gmail.com>
2020-10-04 15:45:08 +0200

    first commit

---
+++ src/Form.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+export default class Form extends Component {
+  initialState = {
+    name: "",
+    job: "",
+  };
+
+  state = this.initialState;
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    console.log({
+      [name]: value,
+    });
+
+    this.setState({
+      [name]: value, //https://medium.com/@bretdoucette/understanding-this-setstate-name-value-a5ef7b4ea2b4
+    });
+  };
+
+  submitForm = () => {
+    this.props.handleSubmit(this.state);
+    this.setState(this.initialState);
+  };
+
+  render() {
+    const { name, job } = this.state;
+
+    return (
+      <form>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="job">Job</label>
+        <input
+          type="text"
+          name="job"
+          id="job"
+          value={job}
+          onChange={this.handleChange}
+        />
+        <input type="button" value="Submit" onClick={this.submitForm} />
+      </form>
+    );
+  }
+}
